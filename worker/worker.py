f6ff01863801164e708b058d0af8b56bacfbc0dc
prox1m1ty <h3x1c4t@gmail.com>
2026-05-30 18:36:28 +0300

    global fix 6

--- worker/worker.py
+++ worker/worker.py
@@ -2,16 +2,13 @@ import os
 from redis import Redis
 from rq import Worker, Queue, Connection
 
-redis_conn = Redis.from_url(
-    os.getenv(
-        "REDIS_URL",
-        "rediss://default:gQAAAAAAAXNTAAIgcDE1MjhiNzJiZmY4OGI0NWQxOTRlMzA1OTBmM2NiMTY5Nw@nice-lacewing-95059.upstash.io:6379"
-    )
-)
+REDIS_URL = "rediss://default:gQAAAAAAAXNTAAIgcDE1MjhiNzJiZmY4OGI0NWQxOTRlMzA1OTBmM2NiMTY5Nw@nice-lacewing-95059.upstash.io:6379"
+
+redis_conn = Redis.from_url(os.getenv("REDIS_URL"))
 
 listen = ["default"]
 
 if __name__ == "__main__":
     with Connection(redis_conn):
         worker = Worker(map(Queue, listen))
-        worker.work()
+        worker.work()        
